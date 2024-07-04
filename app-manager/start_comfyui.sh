ad70c102cb912814ba1b64c80e207af2a20e8b3d
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-04 08:11:08 +0200

    Added custom app manager scripts to start and stop ComfyUI

---
+++ app-manager/start_comfyui.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export PYTHONUNBUFFERED=1
+cd /workspace/ComfyUI
+source venv/bin/activate
+TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
+export LD_PRELOAD="${TCMALLOC}"
+python3 main.py --listen 0.0.0.0 --port 3001 > /workspace/logs/comfyui.log 2>&1 &
+deactivate
